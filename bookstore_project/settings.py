e803fdbdf973f4090e68bb0d01364b8441ffc1fc
Elikibs <elishakibet67@gmail.com>
2024-04-07 23:52:39 +0300

    update settings.py

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -172,4 +172,4 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 # Email configuration
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBacken"
